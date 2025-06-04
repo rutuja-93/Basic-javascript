dc9ae84a2c5857799a6041da0b7c2281f50c9386
rutuja-93 <rutujazaware87@gmail.com>
2025-06-04 08:44:35 +0530

    new updation

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,50 +1,68 @@
 // //DOM[document object miodel]= tree like structure of content of our website
-// //
+
 
 
 // //how to select 
-// //1 using id selector= single element
-// const selectidH1= document.getElementById("my=h1");
-
-// //2 using class= HTML collection-->array
-// const para=document.getElementsByClassName("para");
-
-// //3 tag selector= HTML collection-->array
-
-// const paracollection= document.getElementsByTagName("p");
-
-// //4 Query Selector= element
-
-// const para1=document.querySelector(".my-h1");
-// //console.log(para1);
-
-// //5 QuerySelectorAll= nodelist
-// const paras=document.querySelectorAll(".para");
-// console.log(paras);
+1 id selector= used to select a single HTML element by its ID.
+const selectedH1= document.getElementById("myh1");
+console.log(selectedH1);
 
 
-// //Dom Manipulation = change
+// //2 using class= used to select one or more HTML elements that have the same class name.
+const para=document.getElementsByClassName("para");
+console.log(para)
+console.log(para[2]);//to select singlr para
 
-// // change content
+ //3 tag selector= used to select all HTML elements of a specific tag name like array
 
-// const selectedH1=document.getElementById("my")
+ const paracollection= document.getElementsByTagName("p");
+console.log(paracollection);
+paracollection[1].textContent="this para is updated";//to update the 2nd para
 
-// // 1 innerHTML= add elment
 
-// console.log(selectedH1.innerHTML);
-// selectedH1.innerHTML="DOM 2"
+// 4 Query Selector= used to select the first matching HTML element using CSS-style selectors.
+// document.querySelector("#p")	= Element with id="p"
+//document.querySelector(".p")	= Element with class="p"
+//document.querySelector("p") = First <p> tag in the document
 
-// // 2 textContent= add text
+const para1=document.querySelector("#myh1");
+console.log(para1);
 
-// console.log(selectedH1.textContent);
+// //5 QuerySelectorAll=  used to select all matching HTML elements based on a CSS selector.
+const paras=document.querySelectorAll(".para");
+console.log(paras);
+
+
+
+
+
+
+// Dom Manipulation = DOM Manipulation is the process of changing the structure, style, or content of the HTML document using JavaScript after the page has loaded.
+
+ // how to change content :
+
+// 1 innerHTML= innerHTML is a JavaScript property that allows you to get or set the HTML content inside an element as a string.
+
+const selectedH1=document.getElementById("myh1")
+console.log(selectedH1.innerHTML);
+selectedH1.innerHTML=" Updated DOM 2"; //to change the content
+
+// 2 textContent= add text
+
+const selectedh1=document.getElementById("my1")
+console.log(selectedh1.textContent);
+selectedh1.textContent="updated h1 using text content";
 
 // //3 innerText= add text
 
-// console.log(selectedH1.innerText);
+const selecth1=document.getElementById("myh1")
+console.log(selecth1.innerText);
+selecth1.innerText="updated h1 using inner text";
 
 
-// when we click on button then html content will be change
-const selectedH1=document.getElementById("my1")
+// 1  when we click on button then html content will be change
+
+const content=document.getElementById("my1")
 function ChangeContent(){
     const chnage=prompt("enter ");
     selectedH1.innerHTML="This is first dom manipulation"
