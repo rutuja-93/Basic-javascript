25b3619162eff79e620b9050670d6bb6801f8cee
rutuja-93 <rutujazaware87@gmail.com>
2025-06-05 18:30:21 +0530

    today

--- javascript/inc-dec.js
+++ javascript/inc-dec.js
@@ -1,9 +1,11 @@
 let h1=document.getElementById("number")
 let count=0;
 function decrease(){
+    if(count==0){
+        return;
+    }
     count--
     h1.innerHTML=count;
-
 }
 
 function increase(){
