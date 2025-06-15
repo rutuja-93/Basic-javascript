bdc502a0e1bcb2eb4700359e485aabf4af1d4ea9
rutuja-93 <rutujazaware87@gmail.com>
2025-06-15 15:29:34 +0530

    logic Building Practice

--- javascript/dom/to-do.js
+++ javascript/dom/to-do.js
@@ -10,7 +10,8 @@ const para=document.createElement("p");
 
 para.innerHTML= input.value;
 
-//appendChild= adding element using js
+//appendChild= adding single element at a time using js
+//append= adding multiple element at a time using js
 
 const parent=document.getElementById("box");
 
@@ -25,5 +26,5 @@ function deletepara(){
     if(lastpara)
     parent.removeChild(lastpara);
 }
-//remove=to remove single element
-//removeChild=to remove child element
+//remove=to remove single element, when you have the element and just want to remove it.
+//removeChild=to remove child element, when you're already working with the parent and want to remove a specific child.
